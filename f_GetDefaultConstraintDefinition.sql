afe1a860101b1483a75d6c6f51c9e9016fb1f1ed
Reyhan <reyhan_uciha@yahoo.com>
2021-01-06 14:41:18 +0700

    Create f_GetDefaultConstraintDefinition.sql

---
+++ f_GetDefaultConstraintDefinition.sql
@@ -0,0 +1,20 @@
+CREATE FUNCTION [tSQL].[GetDefaultConstraintDefinition] (
+	@ObjectId INT,
+	@ColumnId INT,
+	@ReturnDetails BIT
+)
+RETURNS TABLE
+AS
+RETURN
+
+SELECT COALESCE(IsDefault, 0) AS IsDefault,
+  COALESCE(DefaultDefinition, '') AS DefaultDefinition
+    FROM (SELECT 1) X(X)
+       LEFT JOIN (SELECT 1 AS IsDefault,' DEFAULT ' + 
+                    definition AS DefaultDefinition,
+                      parent_object_id, parent_column_id
+		  FROM sys.default_constraints
+                 ) A
+ON A.parent_object_id = @ObjectId
+AND A.parent_column_id = @ColumnId
+AND @ReturnDetails = 1;
