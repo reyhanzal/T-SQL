2d6899fb78fea2d8a6381ab849c27e26dca9b02b
Reyhan <reyhan_uciha@yahoo.com>
2021-01-06 14:08:24 +0700

    Create f_GetDataTypeOrComputedColumnDefinition.sql

---
+++ f_GetDataTypeOrComputedColumnDefinition.sql
@@ -0,0 +1,25 @@
+CREATE FUNCTION [tSQL].[GetDataTypeOrComputedColumnDefinition] (
+	@UserTypeId INT,
+	@MaxLength INT,
+	@Precision INT,
+	@Scale INT,
+	@CollationName NVARCHAR(MAX),
+	@ObjectId INT,
+	@ColumnId INT,
+	@ReturnDetails BIT
+)
+RETURNS TABLE
+AS
+RETURN 
+
+SELECT COALESCE(IsComputedColumn, 0) AS IsComputedColumn,
+ COALESCE(ComputedColumnDefinition, TypeName) AS ColumnDefinition
+   FROM tSQL.GetFullTypeName(@UserTypeId, @MaxLength, @Precision, @Scale, @CollationName)
+     LEFT JOIN (SELECT 1 AS IsComputedColumn,' AS '+ definition + CASE WHEN is_persisted = 1 
+		  THEN ' PERSISTED' ELSE '' 
+		    END AS ComputedColumnDefinition,object_id,column_id
+		FROM sys.computed_columns 
+               ) A
+ON A.object_id = @ObjectId
+AND A.column_id = @ColumnId
+AND @ReturnDetails = 1;
