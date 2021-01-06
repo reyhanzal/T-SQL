9c6c77ce49802aa1719585a8ceb9219af635c6c8
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-01-06 13:52:21 +0700

    Update f_GetConstraintType.sql

--- f_GetConstraintType.sql
+++ f_GetConstraintType.sql
@@ -5,7 +5,8 @@ CREATE FUNCTION [tSQL].[GetConstraintType](
 RETURNS TABLE
 AS
 RETURN
-  SELECT object_id,type,type_desc
-    FROM sys.objects 
-     WHERE object_id = OBJECT_ID(SCHEMA_NAME(schema_id)+'.'+@ConstraintName)
-       AND parent_object_id = @TableObjectId;
+                        
+SELECT object_id,type,type_desc
+  FROM sys.objects 
+    WHERE object_id = OBJECT_ID(SCHEMA_NAME(schema_id)+'.'+@ConstraintName)
+      AND parent_object_id = @TableObjectId;
