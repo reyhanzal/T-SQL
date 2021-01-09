06f8b33723a3af1c047dca791d4bef7616abe836
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-01-09 13:11:12 +0700

    Update Transpose.sql

--- Transpose.sql
+++ Transpose.sql
@@ -4,6 +4,8 @@ SELECT [1] AS 'DB_SOURCE', [2] AS 'DB_DESTINATION', [3] AS 'TABLE_SRC', [4] AS '
          FROM [dbo].[FnSplitString2]('[Your_DB_Name], [Your_DB_Name], [Your_Table_Name], [Your_Table_Name]', ',')
 ) A
 PIVOT
-(MIN(Item)
-FOR Row_Num IN ([1],[2],[3],[4]))
+(
+  MIN(Item)
+  FOR Row_Num IN ([1],[2],[3],[4])
+)
 AS B;
