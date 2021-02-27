b14207ecb0f2bf43c5ce0cdb018d08eab956a346
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-02-27 15:22:46 +0700

    Update f_CountDay.sql

--- f_CountDay.sql
+++ f_CountDay.sql
@@ -1,17 +1,18 @@
 CREATE FUNCTION [dbo].[f_CountDay]
 (
-	@DateStart DATETIME,
-	@DateEnd DATETIME
+   @DateStart DATETIME,
+   @DateEnd DATETIME
 )
 RETURN INT
 AS
 BEGIN
-	DECLARE @CountDay INT
-	SET @CountDay = -1
+   DECLARE @CountDay INT
+   SET @CountDay = -1
 	
-	WHILE @DateStart <= @DateEnd 
-	BEGIN
-		SET @CountDay = @CountDay + 1
-	END
-	RETURN (@CountDay)
+   WHILE @DateStart <= @DateEnd 
+   BEGIN
+      SET @CountDay = @CountDay + 1
+   END
+
+   RETURN (@CountDay)
 END
