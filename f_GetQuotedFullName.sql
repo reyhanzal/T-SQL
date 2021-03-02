ea023ee0a5175e807fd4ac11b1f41320cbeba1a4
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-03-02 17:00:24 +0700

    Create f_GetQuotedFullName.sql

---
+++ f_GetQuotedFullName.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION [tSQLs].[GetQuotedFullName] (
+      @Objectid INT
+)
+RETURNS NVARCHAR(517)
+AS
+BEGIN
+    DECLARE @QuotedName NVARCHAR(517);
+    SELECT @QuotedName = QUOTENAME(OBJECT_SCHEMA_NAME(@Objectid)) + '.' + QUOTENAME(OBJECT_NAME(@Objectid));
+    RETURN @QuotedName;
+END
