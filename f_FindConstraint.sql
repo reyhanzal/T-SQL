63d90476e216eca3c7c86ccf58079da7a3696bd4
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-03-09 21:30:08 +0700

    Update f_FindConstraint.sql

--- f_FindConstraint.sql
+++ f_FindConstraint.sql
@@ -1,14 +1,15 @@
-CREATE FUNCTION [tSQLs].[FindConstraint] (
-  @TableObjectId INT,
-  @ConstraintName NVARCHAR(MAX)
+CREATE FUNCTION [tSQLs].[FindConstraint]
+(
+    @TableObjectId INT,
+    @ConstraintName NVARCHAR(MAX)
 )
 RETURNS TABLE
 AS
 RETURN
 
 SELECT TOP(1) constraints.object_id AS ConstraintObjectId, type_desc AS ConstraintType
-  FROM sys.objects constraints
-    CROSS JOIN tSQLs.GetOriginalTableInfo(@TableObjectId) orgTbl
-        WHERE @ConstraintName IN (constraints.name, QUOTENAME(constraints.name))
-          AND constraints.parent_object_id = orgTbl.OrgTableObjectId
-             ORDER BY LEN(constraints.name) ASC;
+FROM sys.objects constraints
+CROSS JOIN tSQLs.GetOriginalTableInfo(@TableObjectId) orgTbl
+WHERE @ConstraintName IN (constraints.name, QUOTENAME(constraints.name))
+AND constraints.parent_object_id = orgTbl.OrgTableObjectId
+ORDER BY LEN(constraints.name) ASC;
