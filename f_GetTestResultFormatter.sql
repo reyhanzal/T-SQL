20738ae24e292fd951a63e454d86b0f8e779a0ee
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-03-11 07:31:59 +0700

    Update and rename f_GetTestResultFormatter to f_GetTestResultFormatter.sql

--- f_GetTestResultFormatter.sql
+++ f_GetTestResultFormatter.sql
@@ -7,7 +7,7 @@ BEGIN
   SELECT @FormatterNm = CAST(Val AS NVARCHAR(MAX))
   FROM sys.extended_properties
   WHERE name = N'tSQLs.ResultFormatter'
-   AND major_id = OBJECT_ID('tSQLs.Private_OutputTestResults')
+  AND major_id = OBJECT_ID('tSQLs.Private_OutputTestResults')
   
   SELECT @FormatterNm = COALESCE(@FormatterNm, 'tSQLt.DefaultResultFormatter')
   
