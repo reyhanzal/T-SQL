19ee9ae8390ca7678af58c690c763e1399c0a593
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-03-14 19:54:56 +0700

    Update f_IntToRoman.sql

--- f_IntToRoman.sql
+++ f_IntToRoman.sql
@@ -4,20 +4,21 @@ CREATE FUNCTION [dbo].[f_IntToRoman](
 RETURNS VARCHAR(100)
 AS
 BEGIN
-RETURN Replicate('M', @x/1000)
+RETURN 
+    REPLICATE('M', @x/1000)
      + REPLACE(REPLACE(REPLACE(
-          Replicate('C', @x%1000/100),
-          Replicate('C', 9), 'CM'),
-          Replicate('C', 5), 'D'),
-          Replicate('C', 4), 'CD')
+          REPLICATE('C', @x%1000/100),
+          REPLICATE('C', 9), 'CM'),
+          REPLICATE('C', 5), 'D'),
+          REPLICATE('C', 4), 'CD')
      + REPLACE(REPLACE(REPLACE(
-          Replicate('X', @x%100 / 10),
-          Replicate('X', 9),'XC'),
-          Replicate('X', 5), 'L'),
-          Replicate('X', 4), 'XL')
+          REPLICATE('X', @x%100 / 10),
+          REPLICATE('X', 9),'XC'),
+          REPLICATE('X', 5), 'L'),
+          REPLICATE('X', 4), 'XL')
      + REPLACE(REPLACE(REPLACE(
-          Replicate('I', @x%10),
-          Replicate('I', 9),'IX'),
-          Replicate('I', 5), 'V'),
-          Replicate('I', 4),'IV')
+          REPLICATE('I', @x%10),
+          REPLICATE('I', 9),'IX'),
+          REPLICATE('I', 5), 'V'),
+          REPLICATE('I', 4),'IV')
 END
