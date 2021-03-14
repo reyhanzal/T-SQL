9da9f9bd5f5ac938227d8418f721f2486d570bbc
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-03-14 11:34:50 +0700

    Create s_Print_Error.sql

---
+++ s_Print_Error.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE [dbo].[s_Print_Error] 
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    PRINT 'Error ' + CONVERT(varchar(50), ERROR_NUMBER()) +
+          ', Severity ' + CONVERT(varchar(5), ERROR_SEVERITY()) +
+          ', State ' + CONVERT(varchar(5), ERROR_STATE()) + 
+          ', Procedure ' + ISNULL(ERROR_PROCEDURE(), '-') + 
+          ', Line ' + CONVERT(varchar(5), ERROR_LINE());
+    PRINT ERROR_MESSAGE();
+END
