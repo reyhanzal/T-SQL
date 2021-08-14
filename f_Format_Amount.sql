fdbda6c16393fee94a2461ac14c31c70b93b8afc
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-08-14 19:48:05 +0700

    Update f_Format_Amount.sql

--- f_Format_Amount.sql
+++ f_Format_Amount.sql
@@ -1,11 +1,11 @@
 CREATE FUNCTION [dbo].[f_Format_Amount]
 (
-    @amount NUMERIC(23,8)
+   @amount NUMERIC(23,8)
 )
 RETURN VARCHAR(23)
 AS
 BEGIN
-    DECLARE @ret_amount VARCHAR(23)
-    SET @ret_amount = CONVERT(VARCHAR(23), CAST(@amount AS MONEY), 1)
-    RETURN @ret_amount
+  DECLARE @retAmount VARCHAR(23)
+  SET @retAmount = CONVERT(VARCHAR(23), CAST(@amount AS MONEY), 1)
+  RETURN @retAmount
 END
