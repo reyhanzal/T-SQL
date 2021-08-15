3eefebd2203841c130aee12e5d2957220652f444
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-08-15 07:15:51 +0700

    Update f_GetCleanSchemaName.sql

--- f_GetCleanSchemaName.sql
+++ f_GetCleanSchemaName.sql
@@ -1,15 +1,13 @@
 CREATE FUNCTION [tSQLs].[GetCleanSchemaName]
 (
-    @SchemaName NVARCHAR(MAX), 
-    @ObjectName NVARCHAR(MAX)
+   @SchemaName NVARCHAR(MAX)
+  ,@ObjectName NVARCHAR(MAX)
 )
 RETURNS NVARCHAR(MAX)
 AS
 BEGIN
-    RETURN (SELECT SCHEMA_NAME(schema_id) 
-              FROM sys.objects 
-                WHERE object_id = CASE WHEN ISNULL(@SchemaName,'') in ('','[]')
-                                    THEN OBJECT_ID(@ObjectName)
-                                    ELSE OBJECT_ID(@SchemaName + '.' + @ObjectName)
-                                  END)
+  RETURN (SELECT SCHEMA_NAME(schema_id) FROM sys.objects 
+          WHERE object_id = CASE WHEN ISNULL(@SchemaName,'') in ('','[]')
+                            THEN OBJECT_ID(@ObjectName)
+                            ELSE OBJECT_ID(@SchemaName + '.' + @ObjectName) END)
 END
