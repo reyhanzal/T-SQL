78b2ac0f6df31135d76c7255fd15fed06ea0ec5b
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-11-08 16:48:50 +0700

    Update f_GetIdentityDefinition.sql

--- f_GetIdentityDefinition.sql
+++ f_GetIdentityDefinition.sql
@@ -1,21 +1,24 @@
-CREATE FUNCTION [tSQLs].[GetIdentityDefinition] (
-	@ObjectId INT,
-	@ColumnId INT,
-	@ReturnDetails BIT
+CREATE FUNCTION [tSQLs].[GetIdentityDefinition]
+(
+     @objectId        INT
+    ,@columnId        INT
+    ,@returnDetails   BIT
 )
 RETURNS TABLE
 AS
 RETURN
- 
-SELECT COALESCE(IsIdentity, 0) AS IsIdentityColumn,
- COALESCE(IdentityDefinition, '') AS IdentityDefinition
-FROM (SELECT 1) X(X)
-LEFT JOIN (SELECT 1 AS IsIdentity,
-			' IDENTITY(' + CAST(seed_value AS NVARCHAR(MAX)) + ',' + 
-			CAST(increment_value AS NVARCHAR(MAX)) + ')' AS IdentityDefinition, 
-			OBJECT_ID, column_id
-           FROM sys.identity_columns
-          ) AS A
-ON A.OBJECT_ID = @ObjectId
-AND A.column_id = @ColumnId
-AND @ReturnDetails = 1;
+BEGIN
+
+  SELECT COALESCE(IsIdentity, 0) AS IsIdentityColumn, COALESCE(IdentityDefinition, '') AS IdentityDefinition
+  FROM (SELECT 1) X(X)
+  LEFT JOIN (SELECT 1 AS IsIdentity,
+  	         ' IDENTITY(' + CAST(seed_value AS NVARCHAR(MAX)) + ',' + 
+  	         CAST(increment_value AS NVARCHAR(MAX)) + ')' AS IdentityDefinition, 
+  	         OBJECT_ID, column_id
+             FROM sys.identity_columns
+            ) AS A
+  ON A.OBJECT_ID = @ObjectId
+  AND A.column_id = @ColumnId
+  AND @ReturnDetails = 1;
+  
+END
