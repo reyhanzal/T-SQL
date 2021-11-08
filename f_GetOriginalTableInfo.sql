25482da023c7dc2cc84b091cc9a91e654b1a6994
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-11-08 16:55:00 +0700

    Update f_GetOriginalTableInfo.sql

--- f_GetOriginalTableInfo.sql
+++ f_GetOriginalTableInfo.sql
@@ -1,15 +1,19 @@
-CREATE FUNCTION [tSQLs].[GetOriginalTableInfo] (
-	@TableObjectId INT
+CREATE FUNCTION [tSQLs].[GetOriginalTableInfo]
+(
+     @TableObjectId INT
 )
 RETURNS TABLE
 AS
 RETURN 
+BEGIN
 
-SELECT CAST(value AS NVARCHAR(4000)) OrgTableName,
-        OBJECT_ID(QUOTENAME(OBJECT_SCHEMA_NAME(@TableObjectId)) + '.' +
-        QUOTENAME(CAST(value AS NVARCHAR(4000)))) OrgTableObjectId
-FROM sys.extended_properties
-WHERE class_desc = 'OBJECT_OR_COLUMN'
-AND major_id = @TableObjectId
-AND minor_id = 0
-AND name = 'tSQLs.FakeTable_OrgTableName';
+  SELECT CAST(value AS NVARCHAR(4000)) OrgTableName,
+          OBJECT_ID(QUOTENAME(OBJECT_SCHEMA_NAME(@TableObjectId)) + '.' +
+          QUOTENAME(CAST(value AS NVARCHAR(4000)))) OrgTableObjectId
+  FROM sys.extended_properties
+  WHERE class_desc = 'OBJECT_OR_COLUMN'
+  AND major_id = @TableObjectId
+  AND minor_id = 0
+  AND name = 'tSQLs.FakeTable_OrgTableName';
+  
+END
