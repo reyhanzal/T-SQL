164020eb9879183d080a439b296db215c7817ef1
M. Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-11-20 18:24:00 +0700

    Update f_GetSchemaId.sql

--- f_GetSchemaId.sql
+++ f_GetSchemaId.sql
@@ -1,15 +1,15 @@
 CREATE FUNCTION [tSQLs].[f_GetSchemaId]
 (
-    @SchemaName NVARCHAR(MAX)
+     @SchemaName NVARCHAR(MAX)
 )
 RETURNS INT
 AS
 BEGIN
-  RETURN
-  (
-    SELECT TOP(1) schema_id
-    FROM sys.schemas
-    WHERE @SchemaName IN (name, QUOTENAME(name), QUOTENAME(name, '"'))
-    ORDER BY CASE WHEN name = @SchemaName THEN 0 ELSE 1 END
-  )
+RETURN
+(
+  SELECT TOP(1) schema_id
+  FROM sys.schemas
+  WHERE @SchemaName IN (name, QUOTENAME(name), QUOTENAME(name, '"'))
+  ORDER BY CASE WHEN name = @SchemaName THEN 0 ELSE 1 END
+)
 END
