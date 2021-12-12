cc49cd9e701fc7672013c0c5e03a8439d9737160
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-12 07:35:31 +0700

    Update f_ResolveFakeTableNamesForBackwardCompatibility.sql

--- f_ResolveFakeTableNamesForBackwardCompatibility.sql
+++ f_ResolveFakeTableNamesForBackwardCompatibility.sql
@@ -1,14 +1,16 @@
-CREATE FUNCTION [tSQLs].[ResolveFakeTableNamesForBackwardCompatibility] (
-	@TableName NVARCHAR(MAX),
-	@SchemaName NVARCHAR(MAX)
+CREATE FUNCTION [tSQLs].[ResolveFakeTableNamesForBackwardCompatibility]
+(
+     @TableName    NVARCHAR(MAX)
+    ,@SchemaName   NVARCHAR(MAX)
 )
 RETURNS TABLE AS 
 RETURN
-
-SELECT QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) AS ClnSchemaName,
-QUOTENAME(OBJECT_NAME(object_id)) AS ClnTableName
-FROM (SELECT CASE
+BEGIN
+  SELECT QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) AS ClnSchemaName,
+  QUOTENAME(OBJECT_NAME(object_id)) AS ClnTableName
+  FROM (SELECT CASE
         WHEN @SchemaName IS NULL THEN OBJECT_ID(@TableName)
         ELSE COALESCE(OBJECT_ID(@SchemaName + '.' + @TableName),OBJECT_ID(@TableName + '.' + @SchemaName)) 
-      END object_id
-) A;
+        END object_id
+  ) A
+END
