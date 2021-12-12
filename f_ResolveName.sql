65ad8af724776879ee08f47f5fc51ec79d4c7fe4
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-12 07:39:06 +0700

    Update f_ResolveName.sql

--- f_ResolveName.sql
+++ f_ResolveName.sql
@@ -1,41 +1,44 @@
-CREATE FUNCTION [tSQLs].[ResolveName] (
-	@Name NVARCHAR(MAX)
+CREATE FUNCTION [tSQLs].[ResolveName]
+(
+     @Name NVARCHAR(MAX)
 )
 RETURNS TABLE 
 AS
 RETURN
-
-WITH resolvedNames (
-   ord,
-   schemaId,
-   objectId,
-   quotedSchemaName,
-   quotedObjectName,
-   quotedFullName,
-   isTestClass,
-   isTestCase,
-   isSchema
-) 
-AS
-(
-	SELECT 1, schemaId, NULL, quotedSchemaName, NULL, quotedSchemaName, isTestClass, 0, 1
-	FROM tSQLs.ResolveSchemaName(@Name)
-	UNION ALL
-	SELECT 2, schemaId, objectId, quotedSchemaName, quotedObjectName, quotedFullName, 0, isTestCase, 0
-	FROM tSQLs.ResolveObjectName(@Name)
-	UNION ALL
-	SELECT 3, NULL, NULL, NULL, NULL, NULL, 0, 0, 0
-)
+BEGIN
+  WITH resolvedNames 
+  (
+     ord
+    ,schemaId
+    ,objectId
+    ,quotedSchemaName
+    ,quotedObjectName
+    ,quotedFullName
+    ,isTestClass
+    ,isTestCase
+    ,isSchema
+  ) 
+  AS
+  (
+    SELECT 1, schemaId, NULL, quotedSchemaName, NULL, quotedSchemaName, isTestClass, 0, 1
+    FROM tSQLs.ResolveSchemaName(@Name)
+    UNION ALL
+    SELECT 2, schemaId, objectId, quotedSchemaName, quotedObjectName, quotedFullName, 0, isTestCase, 0
+    FROM tSQLs.ResolveObjectName(@Name)
+    UNION ALL
+    SELECT 3, NULL, NULL, NULL, NULL, NULL, 0, 0, 0
+  )
 	
-SELECT TOP(1) 
-       schemaId, 
-       objectId, 
-       quotedSchemaName, 
-       quotedObjectName, 
-       quotedFullName, 
-       isTestClass, 
-       isTestCase, 
-       isSchema
-FROM resolvedNames
-WHERE schemaId IS NOT NULL OR ord = 3
-ORDER BY ord
+  SELECT TOP(1) 
+      ,schemaId 
+      ,objectId 
+      ,quotedSchemaName 
+      ,quotedObjectName 
+      ,quotedFullName 
+      ,isTestClass 
+      ,isTestCase
+      ,isSchema
+  FROM resolvedNames
+  WHERE schemaId IS NOT NULL OR ord = 3
+  ORDER BY ord
+END  
