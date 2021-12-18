ae325f89edfdd664ece7a443176e0b065f08cfe6
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-18 13:01:53 +0700

    Update f_ResolveObjectName.sql

--- f_ResolveObjectName.sql
+++ f_ResolveObjectName.sql
@@ -1,40 +1,41 @@
-CREATE FUNCTION [tSQLs].[ResolveObjectName] (
-	@Name NVARCHAR(MAX)
+CREATE FUNCTION [tSQLs].[ResolveObjectName]
+(
+   @Name NVARCHAR(MAX)
 )
 RETURNS TABLE 
 AS
 RETURN
 
-WITH abc (
-  schemaId,
-  objectId
+WITH abc
+(
+   schemaId
+  ,objectId
 ) 
 AS 
 (
-	SELECT SCHEMA_ID(OBJECT_SCHEMA_NAME(OBJECT_ID(@Name))),
-				   OBJECT_ID(@Name)
-	),
-	abcWithNames (
-	    schemaId,
-	    objectId,
-	    quotedSchemaName,
-	    quotedObjectName
-	) 
-	AS
-	(
-		SELECT schemaId, objectId,
-			 QUOTENAME(SCHEMA_NAME(schemaId)) AS quotedSchemaName, 
-			 QUOTENAME(OBJECT_NAME(objectId)) AS quotedObjectName
-			 FROM abc
-	)
+  SELECT SCHEMA_ID(OBJECT_SCHEMA_NAME(OBJECT_ID(@Name))), OBJECT_ID(@Name)
+),
+abcWithNames
+(
+    schemaId
+   ,objectId
+   ,quotedSchemaName
+   ,quotedObjectName
+) 
+AS
+(
+  SELECT schemaId, objectId
+   ,QUOTENAME(SCHEMA_NAME(schemaId)) AS quotedSchemaName
+   ,QUOTENAME(OBJECT_NAME(objectId)) AS quotedObjectName
+  FROM abc
+)
 	  
-SELECT schemaId, 
-	   objectId, 
-	   quotedSchemaName,
-	   quotedObjectName,
-	   quotedSchemaName + '.' + 
-	   quotedObjectName AS quotedFullName, 
-	CASE WHEN LOWER(quotedObjectName) LIKE '[[]test%]' 
-	   AND objectId = OBJECT_ID(quotedSchemaName + '.' + quotedObjectName,'P') 
-	THEN 1 ELSE 0 END AS isTestCase
-FROM abcWithNames;
+SELECT schemaId
+ ,objectId 
+ ,quotedSchemaName
+ ,quotedObjectName
+ ,quotedSchemaName + '.' + quotedObjectName AS quotedFullName 
+ ,CASE WHEN LOWER(quotedObjectName) LIKE '[[]test%]' 
+  AND objectId = OBJECT_ID(quotedSchemaName + '.' + quotedObjectName,'P') 
+  THEN 1 ELSE 0 END AS isTestCase
+FROM abcWithNames
