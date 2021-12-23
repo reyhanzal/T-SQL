1ec8208462ac991cd8f2afd9e1a80e31b05e518d
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-24 06:47:54 +0700

    Update f_ResolveSchemaName.sql

--- f_ResolveSchemaName.sql
+++ f_ResolveSchemaName.sql
@@ -1,34 +1,36 @@
-CREATE FUNCTION [tSQLs].[ResolveSchemaName] (
-	@Name NVARCHAR(MAX)
+CREATE FUNCTION [tSQLs].[ResolveSchemaName]
+(
+   @Name NVARCHAR(MAX)
 )
 RETURNS TABLE 
 AS
 RETURN
 
-WITH abc (
-	schemaId
+WITH abc
+(
+  schemaId
 )
 AS
 (
-	SELECT tSQLs.GetSchemaId(@Name)
+  SELECT tSQLs.GetSchemaId(@Name)
 ),
 
-abcWithNames (
-	schemaId,
-	quotedSchemaName
+abcWithNames
+(
+  schemaId
+ ,quotedSchemaName
 ) 
 AS
 (
-	SELECT schemaId, QUOTENAME(SCHEMA_NAME(schemaId))
-    FROM abc
-)
-  
+  SELECT schemaId, QUOTENAME(SCHEMA_NAME(schemaId))
+  FROM abc
+)  
 
-SELECT schemaId, 
-       quotedSchemaName,
-       CASE WHEN EXISTS(SELECT 1 FROM tSQLt.TestClasses WHERE TestClasses.SchemaId = abcWithNames.schemaId)
-         THEN 1
-       ELSE 0
-       END AS isTestClass, 
-         CASE WHEN schemaId IS NOT NULL THEN 1 ELSE 0 END AS isSchema
-FROM abcWithNames;
+SELECT schemaId 
+ ,quotedSchemaName
+ ,CASE WHEN EXISTS(SELECT 1 FROM tSQLt.TestClasses WHERE TestClasses.SchemaId = abcWithNames.schemaId)
+  THEN 1
+  ELSE 0
+  END AS isTestClass
+ ,CASE WHEN schemaId IS NOT NULL THEN 1 ELSE 0 END AS isSchema
+FROM abcWithNames
