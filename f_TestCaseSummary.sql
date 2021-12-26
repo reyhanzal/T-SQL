b7fc752c2512919b8dffd6dd2ded95c57d10ed4e
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-26 09:56:40 +0700

    Update f_TestCaseSummary.sql

--- f_TestCaseSummary.sql
+++ f_TestCaseSummary.sql
@@ -1,25 +1,29 @@
 CREATE FUNCTION [tSQLs].[TestCaseSummary]()
 RETURNS TABLE
 AS
-RETURN 
-WITH A (
-	xCount, 
-	SuccessCount, 
-	FailCount, 
-	ErrorCount
-)
-AS
-(
-	SELECT COUNT(1),
-	   ISNULL(SUM(CASE WHEN Result = 'Success' THEN 1 ELSE 0 END), 0),
-	   ISNULL(SUM(CASE WHEN Result = 'Failure' THEN 1 ELSE 0 END), 0),
-	   ISNULL(SUM(CASE WHEN Result = 'Error' THEN 1 ELSE 0 END), 0)
-	FROM tSQLs.TestResult
+RETURN
+BEGIN
 
-)
+  WITH A
+  (
+    xCount
+   ,SuccessCount 
+   ,FailCount 
+   ,ErrorCount
+  )
+  AS
+  (
+    SELECT COUNT(1)
+     ,ISNULL(SUM(CASE WHEN Result = 'Success' THEN 1 ELSE 0 END), 0)
+     ,ISNULL(SUM(CASE WHEN Result = 'Failure' THEN 1 ELSE 0 END), 0)
+     ,ISNULL(SUM(CASE WHEN Result = 'Error' THEN 1 ELSE 0 END), 0)
+    FROM tSQLs.TestResult
+  )
 
-SELECT 'Test Case Summary: ' + CAST(xCount AS NVARCHAR) + ' test case(s) executed, '+
-  CAST(SuccessCount AS NVARCHAR) + ' succeeded, '+
-  CAST(FailCount AS NVARCHAR) + ' failed, '+
-  CAST(ErrorCount AS NVARCHAR) + ' errored.' Msg,*
-FROM A;
+  SELECT 'Test Case Summary: ' + CAST(xCount AS NVARCHAR) + ' test case(s) executed, '+
+    CAST(SuccessCount AS NVARCHAR) + ' succeeded, '+
+    CAST(FailCount AS NVARCHAR) + ' failed, '+
+    CAST(ErrorCount AS NVARCHAR) + ' errored.' Msg,*
+  FROM A;
+
+END
