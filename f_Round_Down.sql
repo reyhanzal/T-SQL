3df797bf276ff4037b456cf912854f642808a4b6
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2021-12-26 09:52:37 +0700

    Update f_Round_Down.sql

--- f_Round_Down.sql
+++ f_Round_Down.sql
@@ -1,14 +1,14 @@
 CREATE FUNCTION [dbo].[f_Round_Down]
 (
-   @number NUMERIC(23,8),
-   @digit INT
+     @number NUMERIC(23,8)
+    ,@digit INT
 )
 RETURNS NUMERIC(23,8)
 AS
 BEGIN
-	DECLARE @roundnumber NUMERIC(23,8), @roundupnumber NUMERIC(23,8)
+  DECLARE @roundnumber NUMERIC(23,8), @roundupnumber NUMERIC(23,8)
 
-	SET @roundnumber = POWER(10, @digit)
-	SET @roundupnumber = FLOOR(@number * @roundnumber) / @roundnumber
-	RETURN @roundupnumber
+  SET @roundnumber = POWER(10, @digit)
+  SET @roundupnumber = FLOOR(@number * @roundnumber) / @roundnumber
+  RETURN @roundupnumber
 END
