b8fa62421a78a913b4067656e8963bd8458d4014
M Reyhan Zalbina <muhammadreyhanzalbina@gmail.com>
2022-01-09 07:38:48 +0700

    Update s_CheckColumnExists.sql

--- s_CheckColumnExists.sql
+++ s_CheckColumnExists.sql
@@ -6,31 +6,30 @@ CREATE PROCEDURE [dbo].[s_CheckColumnExists]
 )
 AS
 BEGIN
-   SET NOCOUNT ON
+
+SET NOCOUNT ON;
    
-   DECLARE @SQL       AS NVARCHAR(2000)
-   DECLARE @Me        AS NVARCHAR(100)
-   DECLARE @Err_code  AS INT
-   DECLARE @Err_msg   AS NVARCHAR(500)
+  DECLARE @SQL        NVARCHAR(2000)
+  DECLARE @Me         NVARCHAR(100)
+  DECLARE @Err_code   INT
+  DECLARE @Err_msg    NVARCHAR(500)
 		
-   SET @Err_code = 0
-   SET @Me = OBJECT_NAME(@@PROCID) + N' (1102.00) '
+  SET @err_code = 0
+  SET @me = OBJECT_NAME(@@PROCID) + N' (1102.00) '
 
-   BEGIN TRY
-	 SET @SQL = N'SELECT TOP 1 1 FROM [' + @DBName + '].information_schema.columns ' +
+  BEGIN TRY
+    SET @SQL = N'SELECT TOP 1 1 FROM [' + @DBName + '].information_schema.columns ' +
 	            N'WHERE [Table_Name]=' + QUOTENAME(@TblName,'''') + N' AND [Column_Name]=' + QUOTENAME(@ColName, '''')
-	 EXEC (@SQL)
-	 IF @@ROWCOUNT = 1
-	     RETURN 1
-	 ELSE
-	     RETURN 0
-   END TRY
-
-   BEGIN CATCH
-	 SELECT @Err_code = @@error
-	 PRINT @Me + N'SQL statement failed. ' + @SQL + N' ' + Error_Message()
-	 RETURN -1
-   END CATCH
-
-   RETURN 0
+    EXEC (@SQL)
+    
+    IF @@ROWCOUNT = 1 RETURN 1
+    ELSE RETURN 0
+  END TRY
+  BEGIN CATCH
+    SELECT @err_code = @@error
+    PRINT @me + N'SQL statement failed. ' + @SQL + N' ' + Error_Message()
+    RETURN -1
+  END CATCH
+  
+  RETURN 0
 END
